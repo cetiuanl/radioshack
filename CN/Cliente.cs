2038c63be8f1c62f9c86debbffe071a67b95f51e
chuyp11 <chuyp11@gmail.com>
2018-02-03 14:44:44 -0600

    metodo guardar en clase Cliente

--- CN/Cliente.cs
+++ CN/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -93,9 +94,57 @@ namespace CN {
             _RFC = RFC;
             _esActivo = esActivo;
         }
+
+        public Cliente (DataRow fila) {
+            _idCliente = fila.Field<int>("idCliente");
+            _nombre = fila.Field<string>("nombre");
+            _apellidos = fila.Field<string>("apellido");
+            _domicilio = fila.Field<string>("domicilio");
+            _fechaNacimiento = fila.Field<DateTime>("fechaNacimiento");
+            _telefono = fila.Field<string>("telefono");
+            _correo = fila.Field<string>("correo");
+            _RFC = fila.Field<string>("RFC");
+            _esActivo = fila.Field<bool>("esActivo");
+        }
+
         #endregion
 
         #region Funciones
+
+        public bool guardar() {
+            Dictionary<string, object> parametros = new Dictionary<string, object>();
+            
+            parametros.Add("@nombre", this.nombre);
+            parametros.Add("@apellidos", this.apellidos);
+            parametros.Add("@domicilio", this.domicilio);
+            parametros.Add("@fechaNacimiento", this.fechaNacimiento);
+            parametros.Add("@telefono", this.telefono);
+            parametros.Add("@correo", this.correo);
+            parametros.Add("@RFC", this.RFC);
+            
+
+            if(_idCliente > 0) {
+                //Update
+                parametros.Add("@idCliente", this.idCliente);
+                int resultado = CapaDatos.DataBaseHelper.ExecuteNonQuery("dbo.SPCCliente", parametros);
+                if(resultado == 1) {
+                    return true;
+                }
+                return false;
+            } else {
+                //Insert
+                parametros.Add("@esActivo", this.esActivo);
+                return CapaDatos.DataBaseHelper.ExecuteNonQuery("dbo.SPACliente", parametros) == 1;
+            }
+        }
+
+        public bool desactivar() {
+            return false;
+        }
+
+        public bool eliminar() {
+            return false;
+        }
 
         #endregion
 
