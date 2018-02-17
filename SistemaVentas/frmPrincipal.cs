44f09b0a7b24f061603d53c14161cc39266bf2a7
erolando <erolando.avila@aol.com>
2018-02-17 16:25:11 -0600

    cambios

---
+++ SistemaVentas/frmPrincipal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVentas
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void facturasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmFactura form = new frmFactura();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmClientes form = new frmClientes();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void productosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmProductos form = new frmProductos();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void categoriasProductosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCategoriasProducto form = new frmCategoriasProducto();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void rolesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmRoles form = new FrmRoles();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void modoDePagosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmModoPago form = new frmModoPago();
+            form.MdiParent = this;
+            form.Show();
+        }
+
+        private void empleadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmEmpleados form = new frmEmpleados();
+            form.MdiParent = this;
+            form.Show();
+        }
+    }
+}
