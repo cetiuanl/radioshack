c0ec01fc727430ee6beebc54550fc42a43d98466
erolando <erolando.avila@aol.com>
2018-02-17 15:57:56 -0600

    cambios

---
+++ SistemaVentas/frmModoPago.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace SistemaVentas
+{
+    partial class frmModoPago
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvModoPagos = new System.Windows.Forms.DataGridView();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnDesactivar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtIdModoPago = new System.Windows.Forms.TextBox();
+            this.txtDetalles = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModoPagos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvModoPagos
+            // 
+            this.dgvModoPagos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvModoPagos.Location = new System.Drawing.Point(38, 71);
+            this.dgvModoPagos.Name = "dgvModoPagos";
+            this.dgvModoPagos.Size = new System.Drawing.Size(574, 273);
+            this.dgvModoPagos.TabIndex = 0;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(222, 610);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnDesactivar
+            // 
+            this.btnDesactivar.Location = new System.Drawing.Point(336, 610);
+            this.btnDesactivar.Name = "btnDesactivar";
+            this.btnDesactivar.Size = new System.Drawing.Size(75, 23);
+            this.btnDesactivar.TabIndex = 2;
+            this.btnDesactivar.Text = "Eliminar";
+            this.btnDesactivar.UseVisualStyleBackColor = true;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(127, 440);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(240, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtIdModoPago
+            // 
+            this.txtIdModoPago.Location = new System.Drawing.Point(127, 396);
+            this.txtIdModoPago.Name = "txtIdModoPago";
+            this.txtIdModoPago.Size = new System.Drawing.Size(100, 20);
+            this.txtIdModoPago.TabIndex = 4;
+            // 
+            // txtDetalles
+            // 
+            this.txtDetalles.Location = new System.Drawing.Point(127, 483);
+            this.txtDetalles.Name = "txtDetalles";
+            this.txtDetalles.Size = new System.Drawing.Size(240, 20);
+            this.txtDetalles.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 399);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Id Modo Pago:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 446);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 493);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Detalles:";
+            // 
+            // frmModoPago
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 660);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDetalles);
+            this.Controls.Add(this.txtIdModoPago);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnDesactivar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dgvModoPagos);
+            this.Name = "frmModoPago";
+            this.Text = "Modo de pago";
+            this.Load += new System.EventHandler(this.frmModoPago_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModoPagos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvModoPagos;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnDesactivar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtIdModoPago;
+        private System.Windows.Forms.TextBox txtDetalles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ SistemaVentas/frmModoPago.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CN;
+
+namespace SistemaVentas
+{
+    public partial class frmModoPago : Form
+    {
+        public frmModoPago()
+        {
+            InitializeComponent();
+        }
+
+        private void frmModoPago_Load(object sender, EventArgs e)
+        {
+            List<ModoPago> listado = ModoPago.traerTodos(true);
+
+            dgvModoPagos.DataSource = listado;
+            dgvModoPagos.Refresh();
+        }
+    }
+}
